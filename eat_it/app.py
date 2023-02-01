5a7fbfa018eb34202427b0550b0bffb3005a7157
Adam Blady <52865203+edgzoah@users.noreply.github.com>
2023-02-01 15:20:54 +0100

    Update app.py

--- eat_it/app.py
+++ eat_it/app.py
@@ -12,3 +12,21 @@ def ping():
 def create_user() -> Response:
     user = request.json
     return jsonify(user)
+
+@app.get('/users')
+def get_users() -> Response:
+    return Response(status=501)
+
+@app.put('/users/<id>')
+def update_user(id: str) -> Response:
+    user = request.json
+    return jsonify(user)
+
+@app.patch('/users/<id>')
+def patch_user(id: str) -> Response:
+    user = request.json
+    return jsonify(user)
+
+@app.delete('/users/<id>')
+def delete_user(id: str) -> Response:
+    return Response(status=204)
