071d770055cdda12856d48ab5cb6075a3af3a2d5
Adam Blady <adamoblady@gmail.com>
2023-04-05 15:17:27 +0200

    controllersrepositories

--- eat_it/app.py
+++ eat_it/app.py
@@ -1,4 +1,13 @@
 from flask import Flask, Response, request, jsonify
+from eat_it.repositories import UserRepository
+from eat_it.controllers import (
+    AddUserController,
+    DeleteUserController,
+    UpdateUserController,
+    PatchUserController,
+    PutUserController,
+    GetUserController,
+)
 
 app = Flask(__name__)
 
@@ -14,6 +23,8 @@ def ping():
 @app.post('/users')
 def create_user() -> Response:
     user = request.json
+    controller = AddUserController(UserRepository())
+    controller.add(user)
     return jsonify(user)
 
 @app.get('/users')
@@ -23,13 +34,20 @@ def get_users() -> Response:
 @app.put('/users/<id>')
 def update_user(id: str) -> Response:
     user = request.json
+    controller = UpdateUserController(UserRepository())
+    controller.update(user)
     return jsonify(user)
 
 @app.patch('/users/<id>')
 def patch_user(id: str) -> Response:
     user = request.json
+    controller = PatchUserController(UserRepository())
+    controller.patch(user)
     return jsonify(user)
 
 @app.delete('/users/<id>')
 def delete_user(id: str) -> Response:
+    user = request.json
+    controller = DeleteUserController(UserRepository())
+    controller.delete(user)
     return Response(status=204)
